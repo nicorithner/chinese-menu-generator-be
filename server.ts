1a424cd3856d3850237e972a51bfb7ba0e965812
Nico Rithner <nicorithner@gmail.com>
2023-06-13 18:07:32 -0600

    Change index.ts to server.ts and move tsconfig to root

---
+++ server.ts
@@ -0,0 +1,7 @@
+import app from "./src/app";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.listen(port, (): void => console.log(`running on port ${port}`));
