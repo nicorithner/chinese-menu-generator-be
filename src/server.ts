b190da720ea41ad79bdab00455ff138ef61f31de
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-06-14 18:15:04 -0700

    Initialize database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import app from "./app";
 import dotenv from "dotenv";
+import "reflect-metadata"
 dotenv.config();
 
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "postgres",
+})
+
+dataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized successfully.")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+
+//port
 const port = process.env.PORT;
 
 app.listen(port, (): void => console.log(`running on port ${port}`));
