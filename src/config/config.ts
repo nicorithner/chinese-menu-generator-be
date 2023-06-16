f388aac811d6d38318344cc6b435cad6bb115461
Nico Rithner <nicorithner@gmail.com>
2023-06-16 07:51:19 -0600

    Update Datasource file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Ingredient } from "../models/ingredient.entity";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "chinese_menu_dev",
-    synchronize: true,
-    logging: false,
-    entities: [],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "chinese_menu_dev",
+  synchronize: true,
+  logging: true,
+  entities: [Ingredient],
+  migrations: [],
+  subscribers: [],
+});
