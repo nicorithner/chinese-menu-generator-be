84033e154de7ab1923018fc137ab867c21cdfa1f
Nico Rithner <nicorithner@gmail.com>
2023-06-16 07:54:21 -0600

    WIP. Temporary ingredients routes. Patch not working

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,44 @@
-import express, { Request, Response } from 'express';
-const app = express();
+import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/config";
+import { Ingredient } from "./models/ingredient.entity";
 
-app.get('/', (_req: Request, res: Response): Response => {
-  return res.status(200).json({message: 'Hello World!'})
+// create and setup express app
+const app = express();
+app.use(express.json());
+
+// register routes
+app.get("/ingredients", async function (req: Request, res: Response) {
+  const ingredients = await Ingredient.find();
+  res.json(ingredients);
+});
+
+app.get("/ingredients/:id", async function (req: Request, res: Response) {
+  const results = await Ingredient.findBy({ id: +req.params.id });
+  return res.send(results);
+});
+
+app.post("/ingredients", async function (req: Request, res: Response) {
+  const ingredient = await Ingredient.create(req.body);
+  const results = await AppDataSource.getRepository(Ingredient).save(
+    ingredient
+  );
+  return res.send(results);
+});
+
+// app.patch("/ingredients/:id", async function (req: Request, res: Response) {
+//   const ingredient = await Ingredient.findOneBy({ id: req.body.id });
+
+//   Ingredient.merge(ingredient, req.body);
+//   const results = await Ingredient.update(ingredient?.id, req.body);
+
+//   return res.send(results);
+// });
+
+app.delete("/ingredients/:id", async function (req: Request, res: Response) {
+  const results = await AppDataSource.getRepository(Ingredient).delete(
+    req.params.id
+  );
+  return res.send(results);
 });
 
 export default app;
