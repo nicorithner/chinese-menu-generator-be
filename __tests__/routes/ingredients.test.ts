382f0f763a794efef18f446358e5b4d3b244643d
Nico Rithner <nicorithner@gmail.com>
2023-06-17 13:07:09 -0600

    Add datasource instanziation with test db
    Add test for '/ingredients'
    it finds all ingredients in test db

--- __tests__/routes/ingredients.test.ts
+++ __tests__/routes/ingredients.test.ts
@@ -1,20 +1,49 @@
 import request from "supertest";
-import { Express } from "express-serve-static-core";
+import { AppDataSource } from "../../src/config/config";
+import { Ingredient } from "../../src/models/ingredient.entity";
 import app from "../../src/app";
 
-let server: Express;
+let connection: any;
 
-describe("Ingredients Routes", () => {
-  beforeAll(() => {
-    server = app;
+describe("Ingredients Endpoints", () => {
+  beforeAll(async () => {
+    // set up test db
+    AppDataSource.setOptions({
+      database: "chinese_menu_test",
+      entities: [Ingredient],
+      synchronize: true,
+      dropSchema: true,
+    });
+    connection = await AppDataSource.initialize();
+    await connection.synchronize(true);
+
+    // create some ingredients
+    const ingredient1 = await Ingredient.create({ name: "kale" });
+    const ingredient2 = await Ingredient.create({ name: "apple" });
+    await AppDataSource.getRepository(Ingredient).save(ingredient1);
+    await AppDataSource.getRepository(Ingredient).save(ingredient2);
   });
 
-  it("should return 200", (done) => {
-    request(server)
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("'/ingredients', Gets all ingredients", (done) => {
+    const response = request(app)
       .get("/ingredients")
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
+        expect(res.body).toMatchObject([
+          {
+            id: 1,
+            name: "kale",
+          },
+          {
+            id: 2,
+            name: "apple",
+          },
+        ]);
         done();
       });
   });
