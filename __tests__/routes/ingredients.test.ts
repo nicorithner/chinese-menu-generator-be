371739341947ea55b3d332a79e721d9855257d67
Nico Rithner <nicorithner@gmail.com>
2023-06-17 11:57:27 -0600

    WIP. Start tests for ingredients

--- __tests__/routes/ingredients.test.ts
+++ __tests__/routes/ingredients.test.ts
@@ -1,21 +1,20 @@
 import request from "supertest";
 import { Express } from "express-serve-static-core";
-import app from "../src/app";
+import app from "../../src/app";
 
 let server: Express;
 
-describe('APP should say "Hello World!"', () => {
+describe("Ingredients Routes", () => {
   beforeAll(() => {
     server = app;
   });
 
   it("should return 200", (done) => {
     request(server)
-      .get("/")
+      .get("/ingredients")
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.body).toMatchObject({ message: `Hello World!` });
         done();
       });
   });
