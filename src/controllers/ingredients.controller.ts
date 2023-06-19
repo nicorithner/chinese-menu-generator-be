52a966b676ab48a68f6fa3ea0cb469adff67713f
Nico Rithner <nicorithner@gmail.com>
2023-06-19 15:28:24 -0600

    Add type to err

--- src/controllers/ingredients.controller.ts
+++ src/controllers/ingredients.controller.ts
@@ -9,7 +9,7 @@ export const findAllIngredients = async (req: Request, res: Response) => {
   try {
     const ingredients = await Ingredient.find();
     res.json(ingredients);
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).send({
       message:
         err.message || "Something went wrong while retrieving ingredients",
@@ -21,7 +21,7 @@ export const findIngredientById = async (req: Request, res: Response) => {
   try {
     const result = await Ingredient.findBy({ id: +req.params.id });
     return res.send(result);
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).send({
       message:
         err.message || `Something went wrong while retrieving ingredient`,
@@ -39,7 +39,7 @@ export const createIngredient = async (req: Request, res: Response) => {
       message: `Ingredient id: ${result.id} created successfully`,
       result,
     });
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).send({
       message: err.message || "Something went wrong while creating ingredient",
     });
@@ -52,7 +52,7 @@ export const updateIngredient = async function (req: Request, res: Response) {
     await Ingredient.update(ingredient[0].id, req.body);
     const updated = await Ingredient.findBy({ id: +req.params.id });
     return res.send(updated);
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).send({
       message: err.message || "Something went wrong while updating ingredient",
     });
@@ -65,7 +65,7 @@ export const deleteIngredient = async (req: Request, res: Response) => {
       req.params.id
     );
     return res.send({message: "Successfully deleted ingredient", results });
-  } catch (err) {
+  } catch (err: any) {
     res.status(500).send({
       message: err.message || "Something went wrong while deleting ingredient",
     });
