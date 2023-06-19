fcfc18ebd30c5e78d8e1142b7dbe2c3d67821bdd
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-06-19 10:35:11 -0700

    Add the menu entity

---
+++ src/models/menu.entity.ts
@@ -0,0 +1,27 @@
+import {
+    BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToOne
+} from "typeorm"
+
+import { User } from "./user.entity"
+
+
+@Entity()
+export class Menu extends BaseEntity {
+
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+
+    @Column()
+    name: string
+
+
+    @Column()
+    user_id: number
+
+
+    @ManyToOne(() => User, (user) => user.menus, { onDelete: "CASCADE" })
+    user: User
+
+}
