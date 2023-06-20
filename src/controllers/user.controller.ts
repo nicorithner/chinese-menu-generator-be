c975aadf02f08cac4f8eb6fc7fb121d5ba2e1453
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-06-20 14:42:49 -0700

    Add error handling in user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,35 +7,73 @@ const app = express();
 app.use(express.json())
 
 export const findAllUsers = async (req: Request, res: Response) => {
-    const users = await User.find();
-    res.json(users);
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err: any) {
+        res.status(500).send({
+            message:
+                err.message || "Something went wrong while retrieving users",
+        });
+    }
 };
 
-export const findUser = async function (req: Request, res: Response) {
-    const results = await User.findBy({ id: +req.params.id });
-    return res.send(results);
+export const findUserByID = async function (req: Request, res: Response) {
+    try {
+        const results = await User.findBy({ id: +req.params.id });
+        return res.send(results);
+    } catch (err: any) {
+        res.status(500).send({
+            message:
+                err.message || "Something went wrong while retrieving a user",
+        });
+    }
 };
 
 
 export const createUser = async function (req: Request, res: Response) {
-    const user = await User.create(req.body);
-    const results = await AppDataSource.getRepository(User).save(user);
-    return res.send(results);
+    try {
+        const user = await User.create(req.body);
+        const result = await AppDataSource.getRepository(User).save(user);
+        return res.status(200).send({
+            message: `User id: ${result.id} created successfully`, result
+        });
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while creating user",
+        });
+    }
 };
 
 export const updateUser = async function (req: Request, res: Response) {
-    const user = await User.findBy({ id: +req.params.id });
-    await User.update(user[0].id, req.body);
-    const updated = await User.findBy({ id: +req.params.id });
+    try {
+        const user = await User.findBy({ id: +req.params.id });
+        await User.update(user[0].id, req.body);
+        const updated = await User.findBy({ id: +req.params.id });
 
-    return res.send(updated);
+        return res.send(updated);
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while updating user",
+        });
+    }
 };
 
 export const deleteUser = async function (req: Request, res: Response) {
-
-    await AppDataSource.getRepository(Menu).delete({ user_id: +req.params.id });
-    const results = await AppDataSource.getRepository(User).delete(
-        req.params.id
-    );
-    return res.send(results);
+    try {
+        await AppDataSource.getRepository(Menu).delete({ user_id: +req.params.id });
+        const results = await AppDataSource.getRepository(User).delete(
+            req.params.id
+        );
+        return res.send({ message: "Successfully deleted user", results });
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while deleting user",
+        });
+    }
 };
+
+export default app;
