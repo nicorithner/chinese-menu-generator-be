fc57a36d210ae1508e0956b2fac8115de698ec8f
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-06-20 14:42:33 -0700

    Add error handling in menu controller

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -6,30 +6,70 @@ const app = express();
 app.use(express.json());
 
 export const findAllMenus = async (req: Request, res: Response) => {
-    const menus = await Menu.find();
-    res.json(menus);
+    try {
+        const menus = await Menu.find();
+        res.json(menus);
+    } catch (err: any) {
+        res.status(500).send({
+            message:
+                err.message || "Something went wrong while retrieving menus",
+        });
+    }
 };
 
 export const findMenuById = async (req: Request, res: Response) => {
-    const menu = await Menu.findBy({ id: +req.params.id });
-    return res.send(menu);
+    try {
+        const menu = await Menu.findBy({ id: +req.params.id });
+        return res.send(menu);
+    } catch (err: any) {
+        res.status(500).send({
+            message:
+                err.message || "Something went wrong while retrieving a menu",
+        });
+    }
 }
 
 export const createMenu = async (req: Request, res: Response) => {
-    const menu = await Menu.create(req.body);
-    const results = await AppDataSource.getRepository(Menu).save(menu);
-    return res.send(results);
-
+    try {
+        const menu = await Menu.create(req.body);
+        const results = await AppDataSource.getRepository(Menu).save(menu);
+        return res.status(200).send({
+            message: `Menu id: ${menu.id} created successfully`, menu
+        });
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while creating menu",
+        });
+    }
 }
+
 export const updateMenu = async (req: Request, res: Response) => {
-    const menuToUpdate = await Menu.findBy({ id: +req.params.id });
-    await Menu.update(menuToUpdate[0].id, req.body);
-    const menuUpdated = await Menu.findBy({ id: +req.params.id })
-    return res.send(menuUpdated);
-
+    try {
+        const menuToUpdate = await Menu.findBy({ id: +req.params.id });
+        await Menu.update(menuToUpdate[0].id, req.body);
+        const menuUpdated = await Menu.findBy({ id: +req.params.id })
+        return res.send(menuUpdated);
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while updating menu",
+        });
+    }
 }
+
 export const deleteMenu = async (req: Request, res: Response) => {
-    const results = await AppDataSource.getRepository(Menu).delete(req.params.id);
-    return res.send(results);
-}
+    try {
+        const results = await AppDataSource.getRepository(Menu).delete(req.params.id);
+        return res.send({ message: "Successfully deleted menu", results });
+    } catch (err: any) {
+        res.status(500).send({
+            message: err.message ||
+                "Something went wrong while deleting menu",
+        });
+    }
+};
 
+
+
+export default app;
