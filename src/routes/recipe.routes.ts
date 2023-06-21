2d3bf63534ac31a8823c640de692f2d410bbb037
Nico Rithner <nicorithner@gmail.com>
2023-06-21 16:03:29 -0600

    Create route to find recipe ingredients

--- src/routes/recipe.routes.ts
+++ src/routes/recipe.routes.ts
@@ -4,6 +4,7 @@ import {
   createRecipe,
   updateRecipe,
   deleteRecipe,
+  findRecipeIngredients,
 } from "../controllers/recipe.controller";
 
 module.exports = (app) => {
@@ -21,4 +22,7 @@ module.exports = (app) => {
 
   // Delete a network with id
   app.delete("/recipes/:id", deleteRecipe);
+
+  // Find recipe's ingredients
+  app.get("/recipes/:id/ingredients", findRecipeIngredients);
 };
