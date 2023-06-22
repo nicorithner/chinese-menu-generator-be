a1cfcf490f0a19d4299d8cbef0c20b10a33d91a5
Lingling Gan <linglinggan@Linglings-MacBook-Pro.local>
2023-06-21 18:58:42 -0700

    Add menu not found error handling block

--- src/controllers/menu.controller.ts
+++ src/controllers/menu.controller.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 export const findAllMenus = async (req: Request, res: Response) => {
     try {
         const menus = await Menu.find();
+        if (menus.length === 0) throw new Error("No menus found!")
         res.json(menus);
     } catch (err: any) {
         res.status(500).send({
@@ -19,7 +20,8 @@ export const findAllMenus = async (req: Request, res: Response) => {
 
 export const findMenuById = async (req: Request, res: Response) => {
     try {
-        const menu = await Menu.findBy({ id: +req.params.id });
+        const menu = await Menu.findOneBy({ id: +req.params.id });
+        if (!menu) throw new Error(`No menu with id of ${+req.params.id} found!`)
         return res.send(menu);
     } catch (err: any) {
         res.status(500).send({
@@ -46,8 +48,9 @@ export const createMenu = async (req: Request, res: Response) => {
 
 export const updateMenu = async (req: Request, res: Response) => {
     try {
-        const menuToUpdate = await Menu.findBy({ id: +req.params.id });
-        await Menu.update(menuToUpdate[0].id, req.body);
+        const menuToUpdate = await Menu.findOneBy({ id: +req.params.id });
+        if (!menuToUpdate) throw new Error(`Menu with id of ${+req.params.id} can not be found!`)
+        await Menu.update(menuToUpdate.id, req.body);
         const menuUpdated = await Menu.findBy({ id: +req.params.id })
         return res.send(menuUpdated);
     } catch (err: any) {
@@ -60,8 +63,11 @@ export const updateMenu = async (req: Request, res: Response) => {
 
 export const deleteMenu = async (req: Request, res: Response) => {
     try {
-        const results = await AppDataSource.getRepository(Menu).delete(req.params.id);
-        return res.send({ message: "Successfully deleted menu", results });
+
+        const menuToDelete = await AppDataSource.getRepository(Menu).findOneBy({ id: +req.params.id });
+        if (!menuToDelete) throw new Error(`Menu with id of ${+req.params.id} can not be found!`)
+        const result = await AppDataSource.getRepository(Menu).delete(req.params.id);
+        return res.send({ message: "Successfully deleted menu", result });
     } catch (err: any) {
         res.status(500).send({
             message: err.message ||
