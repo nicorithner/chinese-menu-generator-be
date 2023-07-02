e10e3dc83864a46fa2dbdd85a503c55c60074100
Nico Rithner <nicorithner@gmail.com>
2023-07-02 15:43:02 -0600

    Remove error "express.default is not a function"
    by adding this snipet to js file.
    Source: https://stackoverflow.com/questions/63744824/getting-express-default-is-not-a-function-error-when-i-run-node-server-in-a-co

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 "use strict";
 exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
 app.use(express_1["default"].json());
 //routes
 app.get("/", function (_req, res) {
